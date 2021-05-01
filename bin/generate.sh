dcd1bcd5fe62988b1ad31329b96b1978b9340d78
PheysX <PheysX@users.noreply.github.com>
2021-05-01 18:07:10 +0200

    Updated php action

--- bin/generate.sh
+++ bin/generate.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo "Adding absolute path to .htaccess: $(pwd)"
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
+if echo "$OSTYPE" | grep -q "^darwin"; then
   sed -i "" -e "s?{ABSOLUTE_DIR_PATH}?$(pwd)?" ./.htaccess
 else
   sed -i -e "s?{ABSOLUTE_DIR_PATH}?$(pwd)?" ./.htaccess
