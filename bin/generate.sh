3082a4f3499bc0cbf12e0eff5d8c2dcadcb8e8d1
PheysX <PheysX@users.noreply.github.com>
2021-05-01 17:51:41 +0200

    Updated php action

--- bin/generate.sh
+++ bin/generate.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
-echo "Adding absolute path to .htaccess.",
-sed -i "" -e "s?{ABSOLUTE_DIR_PATH}?$(pwd)?" ./.htaccess
+echo "Adding absolute path to .htaccess: $(pwd)"
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  sed -i "" -e "s?{ABSOLUTE_DIR_PATH}?$(pwd)?" ./.htaccess
+else
+  sed -i -e "s?{ABSOLUTE_DIR_PATH}?$(pwd)?" ./.htaccess
+fi
+
 echo "Generate .htaccess password:"
 htpasswd -c ./.htpasswd admin
