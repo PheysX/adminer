608749252b384cab555093a92167241209920c6c
PheysX <PheysX@users.noreply.github.com>
2021-05-01 11:32:15 +0200

    Updated to composer version 2, added mac sed support

--- generate.sh
+++ generate.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-echo "Adding absoulte path to .htaccess.",
-sed -i 's?{ABSOLUTE_DIR_PATH}?'$(pwd)'?' ./.htaccess
+#!/usr/bin/env bash
+echo "Adding absolute path to .htaccess.",
+sed -i "" -e "s?{ABSOLUTE_DIR_PATH}?$(pwd)?" ./.htaccess
 echo "Generate .htaccess password:"
 htpasswd -c ./.htpasswd admin
